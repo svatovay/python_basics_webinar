095dc6f1524bf02c81af3a675eaca83b8292b152
svatovay <zombeaver@gmail.com>
2021-08-29 10:38:25 +0500

    correction task_1_2

--- Svatov_Alexey_dz_1/task_1_2.py
+++ Svatov_Alexey_dz_1/task_1_2.py
@@ -1,28 +1,33 @@
 my_list = []
 sum_valid_numbers = 0
-sum_valid_numbers_17 = 0
+sum_valid_numbers_mod = 0
+MOD = 17
 for value in range(1, 1001):
     if value % 2:
         my_list.append(value ** 3)
-
 for number in my_list:
-    number_17 = number + 17
+    number_mod = number + MOD
     sum_numbers = 0
-    sum_numbers_17 = 0
-    str_number = str(number)
-    str_number_17 = str(number_17)
-    idx = 0
-    idx_17 = 0
-    while idx < len(str_number):
-        sum_numbers += int(str_number[idx])
-        idx += 1
+    sum_numbers_mod = 0
+    digit = 1
+    digit_mod = 1
+    digit_number = 0
+    digit_number_mod = 0
+    number_copy = number
+    number_copy_mod = number_mod
+    while number_copy:
+        digit *= 10
+        digit_number = number_copy % digit
+        sum_numbers += digit_number // (digit // 10)
+        number_copy -= digit_number
     if not sum_numbers % 7:
         sum_valid_numbers += number
-    while idx_17 < len(str_number_17):
-        sum_numbers_17 += int(str_number_17[idx_17])
-        idx_17 += 1
-    if not sum_numbers_17 % 7:
-        sum_valid_numbers_17 += number_17
-
+    while number_copy_mod:
+        digit_mod *= 10
+        digit_number_mod = number_copy_mod % digit_mod
+        sum_numbers_mod += digit_number_mod // (digit_mod // 10)
+        number_copy_mod -= digit_number_mod
+    if not sum_numbers_mod % 7:
+        sum_valid_numbers_mod += number_mod
 print(sum_valid_numbers)
-print(sum_valid_numbers_17)
+print(sum_valid_numbers_mod)
