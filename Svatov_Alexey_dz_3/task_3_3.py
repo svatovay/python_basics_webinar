1ebd5b67c1766965cba1e71ef6162c267d1810dd
svatovay <zombeaver@gmail.com>
2021-09-07 21:00:55 +0500

    done task_3_3

--- Svatov_Alexey_dz_3/task_3_3.py
+++ Svatov_Alexey_dz_3/task_3_3.py
@@ -1,9 +1,11 @@
-name_tuple = ('Мария', 'Пётр', 'Марк', 'Серж', 'Сергей')
-dict_names = {}
-for name in name_tuple:
-    if dict_names.get(name[0]):
-        dict_names[name[0]] = list(map(str, name_tuple))
-    else:
-        dict_names.setdefault(name[0], list(map(str, name_tuple)))
-print(dict_names)
-# Доделать!!! значения к ключам должны делать списком (даже, если одно значение)
+def thesaurus(name_tuple):
+    dict_names = {}
+    for name in name_tuple:
+        if dict_names.get(name[0]):
+            dict_names[name[0]].append(name)
+        else:
+            dict_names.setdefault(name[0], [name])
+    return dict_names
+
+
+print(thesaurus(tuple(input('Введите имена через запятую: ').split(', '))))
