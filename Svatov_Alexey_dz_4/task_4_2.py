1485f73e8dd8e457e0e32a59b26f24aed155cec5
svatovay <zombeaver@gmail.com>
2021-09-11 15:45:29 +0500

    done task_4_3

--- Svatov_Alexey_dz_4/task_4_2.py
+++
@@ -1,7 +0,0 @@
-import requests
-import requests.utils as utils
-
-response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-encodings = utils.get_encoding_from_headers(response.headers)
-content = response.content.decode(encoding=encodings)
-print(content)
