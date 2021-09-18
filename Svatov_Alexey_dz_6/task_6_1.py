ad4951dff5644329cff9658515b3ae8bfb2af353
svatovay <zombeaver@gmail.com>
2021-09-18 13:41:06 +0500

    done task_6_3

--- Svatov_Alexey_dz_6/task_6_1.py
+++ Svatov_Alexey_dz_6/task_6_1.py
@@ -1,11 +1,6 @@
 with open('nginx_logs.txt', 'r', encoding='utf-8') as nginx_logs:
-    # logs_list = [line for line in nginx_logs]
-    # a = logs_list[0].split(' ')
     logs_list = []
     for line in nginx_logs:
         _ = line.split(' ')[0], line.split(' ')[5][1:], line.split(' ')[6]
         logs_list.append(_)
-        # logs_list.append(line.split(' ')[0])
-        # logs_list.append(line.split(' ')[5][1:])
-        # logs_list.append(line.split(' ')[6])
-print(logs_list[0:5])
+print(*logs_list, sep='\n')
