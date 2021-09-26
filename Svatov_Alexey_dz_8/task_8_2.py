c9bbcaf3bdde44fa4eee9c727290c80b99f73464
svatovay <zombeaver@gmail.com>
2021-09-26 16:19:14 +0500

    done task_8_2

--- Svatov_Alexey_dz_8/task_8_2.py
+++ Svatov_Alexey_dz_8/task_8_2.py
@@ -1,21 +1,12 @@
 import re
 
-RE_ADDR = re.compile(r'(\d|\.|\:|[0-9A-Fa-f])*')
-RE_RDATETIME = re.compile(r'[^[]+]')
-RE_RTYPE = re.compile(r'[A-Z]{3,7}')
-RE_RRESOURCE = re.compile(r'/downloads/product_1 HTTP/1.1')
-# RE_RCODE = re.compile()
-# RE_RSIZE = re.compile()
-logs_list = []
+RE_ALL = re.compile(r'((?:\d|\.|:|[0-9A-Fa-f])*)\s-\s-\s\['
+                    r'([^[]+)\]\s"'
+                    r'([A-Z]{3,7})\s'
+                    r'(/(?:[a-z]|[A-Z]|\d|_)*/(?:[a-z]|[A-Z]|\d|_)*\s[A-Z]{3,5}/\d\.\d)"\s'
+                    r'([\d]{3})\s'
+                    r'([\d]{1,4})')
 
 with open('nginx_logs.txt', 'r', encoding='utf-8') as nginx_logs:
-    for line in nginx_logs:
-        parsed_raw = RE_ADDR.search(line).group(), \
-                     RE_RDATETIME.search(line).group().replace(']',''), \
-                     RE_RTYPE.search(line).group(), RE_RRESOURCE.search(line)
-        logs_list.append(parsed_raw)
-
-#     for line in nginx_logs:
-#         _ = line.split(' ')[0], line.split(' ')[5][1:], line.split(' ')[6]
-#         logs_list.append(_)
-print(logs_list[0:10])
+    parsed_raw = RE_ALL.findall(nginx_logs.read())
+print(parsed_raw[10])
