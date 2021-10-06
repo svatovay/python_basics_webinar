0e6e9786f227258da972a09191dce73850bb2d8e
svatovay <zombeaver@gmail.com>
2021-10-06 22:37:15 +0500

    add task_11_1
    Need to complite

---
+++ Svatov_Alexey_dz_11/task_11_1.py
@@ -0,0 +1,14 @@
+class Date:
+    def __init__(self, txt):
+        self.txt = txt
+
+    @classmethod
+    def exctractor(cls):
+        pass
+
+    @staticmethod
+    def validator():
+        pass
+
+a = Date('02-10-1991')
+print(a)
