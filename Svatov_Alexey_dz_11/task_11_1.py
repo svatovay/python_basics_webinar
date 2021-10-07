2157136a912debba86161c3d82e824ef755c884a
svatovay <zombeaver@gmail.com>
2021-10-07 20:40:51 +0500

    complete task_11_1

--- Svatov_Alexey_dz_11/task_11_1.py
+++ Svatov_Alexey_dz_11/task_11_1.py
@@ -1,14 +1,22 @@
-class Date:
-    def __init__(self, txt):
-        self.txt = txt
+class Date(object):
+    def __init__(self, new_date):
+        self.new_date = new_date
+
+    def __str__(self):
+        return '.'.join(self.new_date)
 
     @classmethod
-    def exctractor(cls):
-        pass
+    def extractor(cls, input_date):
+        new_date = tuple(input_date.split('-'))
+        date1 = cls(new_date)
+        return date1
 
     @staticmethod
-    def validator():
-        pass
+    def validator(input_date):
+        day, month, year = map(int, input_date.split('.'))
+        return day <= 31 and month <= 12 and year <= 2050
 
-a = Date('02-10-1991')
-print(a)
+
+date = Date.extractor('02-10-1991')
+print(Date.validator(str(date)))
+print(date)
